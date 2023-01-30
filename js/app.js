db5696383d21e9d609cd3f1251c302c25257b3bd
NpsZT0 <hearorsimtang@gmail.com>
2023-01-31 02:04:29 +0700

    HTML&CSS project

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// import * as nav from './navbar.js'
+
+// const navvar = nav.elementNav()
+
+
+// let a = 'hello'
+// const forNavbar = () => {
+//     return navvar
+
+// }
+
+// const main = document.createElement("main")
+// main.innerHTML = forNavbar()
+// document.body.appendChild(main)
