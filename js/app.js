38722e2296959ca7cf60448765617101aaeeefbb
NpsZT0 <hearorsimtang@gmail.com>
2023-02-01 12:36:46 +0700

    html&css finish bmi page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 // import * as nav from './navbar.js'
 
 // const navvar = nav.elementNav()
+// navvar
 
 
-// let a = 'hello'
-// const forNavbar = () => {
-//     return navvar
-
-// }
-
-// const main = document.createElement("main")
-// main.innerHTML = forNavbar()
-// document.body.appendChild(main)
+function bmi(height, weight){
+    const inputHeight = parseInt(document.getElementById(height).value) //ตัวแปรรับค่าจาก input bmi.html ผ่าน id
+    const inputWeight = parseInt(document.getElementById(weight).value)
+    const bmiValue = inputWeight/( Math.pow((inputHeight/100) , 2) )
+    let criterion = ''
+    console.log(bmiValue)
+    if(bmiValue <= 18.5){
+        console.log(bmiValue)
+        criterion = "Underweight!"
+    }
+    else if(bmiValue <= 25){
+        console.log(bmiValue)
+        criterion = "Normal"
+    }
+    else if(bmiValue <= 30){
+        console.log(bmiValue)
+        criterion = "Overweight!"
+    }
+ 
+    document.getElementById("output").innerHTML = "Healthy BMI : " + bmiValue.toFixed(2) + "<br>You are " + criterion
+}
